2caeabcc7b79c3106f6cbb512d39a06cf9f39c88
TeguhSatrio110403 <152392220+TeguhSatrio110403@users.noreply.github.com>
2025-06-24 05:41:35 +0700

    update

--- src/constant/https.jsx
+++ src/constant/https.jsx
@@ -1,3 +1,6 @@
 // export const port = "http://192.168.146.189:3000/"
 // export const port = "http://192.168.120.189:3000/"
 export const port = "https://server-water-sensors.onrender.com/"
+
+// For debugging:
+console.log('API Base URL:', port);
