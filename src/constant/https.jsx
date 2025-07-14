e9a6e161d67357ca82834708e24c84f9088d23ff
TeguhSatrio110403 <152392220+TeguhSatrio110403@users.noreply.github.com>
2025-07-14 13:34:39 +0700

    Update

--- src/constant/https.jsx
+++ src/constant/https.jsx
@@ -1,11 +1,11 @@
 // export const port = "http://192.168.146.189:3000/"
 // export const port = "http://192.168.120.189:3000/"
 
-// export const port = "https://server-water-sensors.onrender.com/"
-// export const ml = "https://pw-brin-ml.onrender.com/"
+ export const port = "https://server-water-sensors.onrender.com/"
+ export const ml = "https://pw-brin-ml.onrender.com/"
 
-export const port = "https://api-watermon.onlimoni.online/"
-export const ml = "https://ml-watermon.onlimoni.online/"
+//export const port = "https://api-watermon.onlimoni.online/"
+//export const ml = "https://ml-watermon.onlimoni.online/"
 
 
 // export const ml = "http://localhost:5000/"
